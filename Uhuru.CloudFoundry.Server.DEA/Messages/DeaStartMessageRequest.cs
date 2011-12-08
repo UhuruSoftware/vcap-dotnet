53d2899caf225c6bc301e69c8f1e9c625d733fe2
Cristi Ancuta <cristian.ancuta@uhurusoftware.com>
2011-12-08 14:26:48 +0200

    Merge remote-tracking branch 'origin/master'

--- Uhuru.CloudFoundry.Server.DEA/Messages/DeaStartMessageRequest.cs
+++ Uhuru.CloudFoundry.Server.DEA/Messages/DeaStartMessageRequest.cs
@@ -149,4 +149,99 @@ namespace Uhuru.CloudFoundry.DEA
             set;
         }
     }
+
+
+    /*
+     * Example:
+     * {"droplet":30,"name":"helloworld","uris":["helloworld.uhurucloud.net"],"runtime":"iis","framework":"net","sha1":"3bce1e6408234c4e2deb7eee8268f4b6123d2b5f","executableFile":"/var/vcap/shared/droplets/3bce1e6408234c4e2deb7eee8268f4b61
+23d2b5f","executableUri":"http://192.168.1.117:9022/staged_droplets/30/3bce1e6408234c4e2deb7eee8268f4b6123d2b5f","version":"3bce1e6408234c4e2deb7eee8268f4b6123d2b5f-2","services":[{"name":"helloservice","type":"database","label":"mssql-2008","vendor":"mssql","version":"2008","tags":["mssql","2008","
+relational"],"plan":"free","plan_option":null,"credentials":{"name":"D4TA4f587f703ee24294808c7aa6df78e4f2","hostname":"192.168.1.116","host":"192.168.1.116","port":1433,"user":"US3RkkyIUnYreXC8","username":"US3RkkyIUnYreXC8","password":"P4SSJ0jwJTg0ojGx"}}],"limits":{"mem":128,"disk":2048,"fds":256}
+,"env":[],"users":["dev@cloudfoundry.org"],"debug":null,"index":0}'
+     */
+
+    /*
+     * 
+     *  "name":"helloservice",
+        "type":"database",
+        "label":"mssql-2008",
+        "vendor":"mssql",
+        "version":"2008",
+        "tags":[
+            "mssql",
+            "2008",
+            " relational"
+        ],
+        "plan":"free",
+        "plan_option":null,
+        "credentials":{
+            "name":"D4TA4f587f703ee24294808c7aa6df78e4f2",
+            "hostname":"192.168.1.116",
+            "host":"192.168.1.116",
+            "port":1433,
+            "user":"US3RkkyIUnYreXC8",
+            "username":"US3RkkyIUnYreXC8",
+            "password":"P4SSJ0jwJTg0ojGx"
+           }
+     */
+    public class StartRequestService : JsonConvertibleObject
+    {
+        public StartRequestService()
+        {
+            Credentials = new StartRequestServiceCredentials();
+        }
+
+        [JsonName("name")]
+        public string ServiceName { get; set; }
+
+        [JsonName("type")]
+        public string ServiceType { get; set; }
+
+        [JsonName("label")]
+        public string Label { get; set; }
+
+        [JsonName("vendor")]
+        public string Vendor { get; set; }
+
+        [JsonName("version")]
+        public string Version { get; set; }
+
+        [JsonName("tags")]
+        public string[] Tags { get; set; }
+
+        [JsonName("plan")]
+        public string Plan { get; set; }
+
+        [JsonName("plan_option")]
+        public Dictionary<string, object> PlanOptions { get; set; }
+
+        [JsonName("credentials")]
+        public StartRequestServiceCredentials Credentials {get; set; }
+
+    }
+
+
+    public class StartRequestServiceCredentials : JsonConvertibleObject
+    {
+        [JsonName("name")]
+        public string InstanceName { get; set; }
+
+        [JsonName("hostname")]
+        public string Hostname { get; set; }
+
+        [JsonName("host")]
+        public string Host { get; set; }
+
+        [JsonName("port")]
+        public int Port { get; set; }
+
+        [JsonName("user")]
+        public string User { get; set; }
+
+        [JsonName("username")]
+        public string Username { get; set; }
+
+        [JsonName("password")]
+        public string Password { get; set; }
+    }
+
 }
