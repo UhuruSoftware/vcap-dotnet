53b8d99773acc6c6dc1a049707cc731b9c2afdf8
viovanov <vlad.iovanov@uhurusoftware.com>
2011-12-14 14:26:39 +0200

    Cleaned up directories and properly configred projects.

---
+++ src/Uhuru.CloudFoundry.DEA.WindowsService/ProjectInstaller.Designer.cs
@@ -0,0 +1,77 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ProjectInstaller.Designer.cs" company="Uhuru Software, Inc.">
+// Copyright (c) 2011 Uhuru Software, Inc., All Rights Reserved
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Uhuru.CloudFoundry.DEA.WindowsService
+{
+    /// <summary>
+    /// Installer class for the Windows Service.
+    /// </summary>
+    public partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Service project installer.
+        /// </summary>
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+
+        /// <summary>
+        /// Service installer.
+        /// </summary>
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+            
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Uhuru .NET Droplet Execution Agent";
+            this.serviceInstaller1.DisplayName = "Uhuru .NET Droplet Execution Agent";
+            this.serviceInstaller1.ServiceName = "DEA";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+    }
+}
