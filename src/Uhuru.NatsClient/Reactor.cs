2ebc56bf5f646b7a8ddd7f262aab790f8c08a13c
viovanov <vlad.iovanov@uhurusoftware.com>
2011-12-27 02:54:09 +0200

    Testing multithreaded nats client.

--- src/Uhuru.NatsClient/Reactor.cs
+++ src/Uhuru.NatsClient/Reactor.cs
@@ -19,6 +19,7 @@ namespace Uhuru.NatsClient
     using System.Text;
     using System.Text.RegularExpressions;
     using System.Threading;
+    using System.Threading.Tasks;
     using Uhuru.NatsClient.Resources;
     using Uhuru.Utilities;
     
@@ -47,6 +48,11 @@ namespace Uhuru.NatsClient
     /// </summary>
     public sealed class Reactor : IDisposable
     {
+        /// <summary>
+        /// Used for running message callbacks on a different thread.
+        /// </summary>
+        private LimitedConcurrencyLevelTaskScheduler messageCallbackScheduler;
+
         /// <summary>
         /// Dictionary containing all subscription 
         /// </summary>
@@ -277,6 +283,8 @@ namespace Uhuru.NatsClient
                 throw new ArgumentNullException("uri");
             }
 
+            this.messageCallbackScheduler = new LimitedConcurrencyLevelTaskScheduler(1);
+            
             this.serverUri = uri;
 
             this.tcpClient = new TcpClient();
@@ -865,7 +873,12 @@ namespace Uhuru.NatsClient
 
             if (nantsSubscription.Callback != null)
             {
-                nantsSubscription.Callback(msg, replyMessage, nantsSubscription.Subject);
+                TaskFactory factory = new TaskFactory(this.messageCallbackScheduler);
+
+                factory.StartNew(() =>
+                    {
+                        nantsSubscription.Callback(msg, replyMessage, nantsSubscription.Subject);
+                    });
             }
 
             // Check for a timeout, and cancel if received >= expected
@@ -883,6 +896,171 @@ namespace Uhuru.NatsClient
         {
             this.tcpClient.GetStream().Close();
             this.tcpClient.Close();
+        }
+
+        /// <summary>
+        /// Provides a task scheduler that ensures a maximum concurrency level while
+        /// running on top of the ThreadPool.
+        /// </summary>
+        private class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
+        {
+            /// <summary>The list of tasks to be executed.</summary>
+            private readonly LinkedList<Task> tasks = new LinkedList<Task>(); // protected by lock(_tasks)
+
+            /// <summary>The maximum concurrency level allowed by this scheduler.</summary>
+            private readonly int maxDegreeOfParallelism;
+            
+            /// <summary>Whether the current thread is processing work items.</summary>
+            [ThreadStatic]
+            private static bool currentThreadIsProcessingItems;
+            
+            /// <summary>Whether the scheduler is currently processing work items.</summary>
+            private int delegatesQueuedOrRunning = 0; // protected by lock(_tasks)
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="LimitedConcurrencyLevelTaskScheduler"/> class with the
+            /// specified degree of parallelism.
+            /// </summary>
+            /// <param name="maxDegreeOfParallelism">The maximum degree of parallelism provided by this scheduler.</param>
+            public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
+            {
+                if (maxDegreeOfParallelism < 1)
+                {
+                    throw new ArgumentOutOfRangeException("maxDegreeOfParallelism");
+                }
+                
+                this.maxDegreeOfParallelism = maxDegreeOfParallelism;
+            }
+
+            /// <summary>Gets the maximum concurrency level supported by this scheduler.</summary>
+            public sealed override int MaximumConcurrencyLevel
+            {
+                get
+                {
+                    return this.maxDegreeOfParallelism;
+                }
+            }
+
+            /// <summary>Attempts to execute the specified task on the current thread.</summary>
+            /// <param name="task">The task to be executed.</param>
+            /// <param name="taskWasPreviouslyQueued">Indicates whether the task was previously queued.</param>
+            /// <returns>Whether the task could be executed on the current thread.</returns>
+            protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+            {
+                // If this thread isn't already processing a task, we don't support inlining
+                if (!currentThreadIsProcessingItems)
+                {
+                    return false;
+                }
+
+                // If the task was previously queued, remove it from the queue
+                if (taskWasPreviouslyQueued)
+                {
+                    this.TryDequeue(task);
+                }
+                
+                // Try to run the task.
+                return this.TryExecuteTask(task);
+            }
+
+            /// <summary>Attempts to remove a previously scheduled task from the scheduler.</summary>
+            /// <param name="task">The task to be removed.</param>
+            /// <returns>Whether the task could be found and removed.</returns>
+            protected sealed override bool TryDequeue(Task task)
+            {
+                lock (this.tasks)
+                {
+                    return this.tasks.Remove(task);
+                }
+            }
+
+            /// <summary>Gets an enumerable of the tasks currently scheduled on this scheduler.</summary>
+            /// <returns>An enumerable of the tasks currently scheduled.</returns>
+            protected sealed override IEnumerable<Task> GetScheduledTasks()
+            {
+                bool lockTaken = false;
+                try
+                {
+                    Monitor.TryEnter(this.tasks, ref lockTaken);
+                    if (lockTaken)
+                    {
+                        return this.tasks.ToArray();
+                    }
+                    else
+                    {
+                        throw new NotSupportedException();
+                    }
+                }
+                finally
+                {
+                    if (lockTaken)
+                    {
+                        Monitor.Exit(this.tasks);
+                    }
+                }
+            }
+
+            /// <summary>Queues a task to the scheduler.</summary>
+            /// <param name="task">The task to be queued.</param>
+            protected sealed override void QueueTask(Task task)
+            {
+                // Add the task to the list of tasks to be processed.  If there aren't enough
+                // delegates currently queued or running to process tasks, schedule another.
+                lock (this.tasks)
+                {
+                    this.tasks.AddLast(task);
+                    if (this.delegatesQueuedOrRunning < this.maxDegreeOfParallelism)
+                    {
+                        ++this.delegatesQueuedOrRunning;
+                        this.NotifyThreadPoolOfPendingWork();
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Informs the ThreadPool that there's work to be executed for this scheduler.
+            /// </summary>
+            private void NotifyThreadPoolOfPendingWork()
+            {
+                ThreadPool.UnsafeQueueUserWorkItem(
+                    _ =>
+                    {
+                        // Note that the current thread is now processing work items.
+                        // This is necessary to enable inlining of tasks into this thread.
+                        currentThreadIsProcessingItems = true;
+                        try
+                        {
+                            // Process all available items in the queue.
+                            while (true)
+                            {
+                                Task item;
+                                lock (tasks)
+                                {
+                                    // When there are no more items to be processed,
+                                    // note that we're done processing, and get out.
+                                    if (tasks.Count == 0)
+                                    {
+                                        --delegatesQueuedOrRunning;
+                                        break;
+                                    }
+
+                                    // Get the next item from the queue
+                                    item = tasks.First.Value;
+                                    tasks.RemoveFirst();
+                                }
+
+                                // Execute the task we pulled out of the queue
+                                base.TryExecuteTask(item);
+                            }
+                        }
+                        finally
+                        {
+                            // We're done processing items on the current thread
+                            currentThreadIsProcessingItems = false;
+                        }
+                    },
+                    null);
+            }
         }
     }
 }
