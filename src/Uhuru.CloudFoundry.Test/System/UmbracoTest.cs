78cf09aa6dd496c2e27d35b7f2e3bfab642cec25
viovanov <vlad.iovanov@uhurusoftware.com>
2011-12-30 20:32:25 +0200

    Removed ignore attribute for umbraco stress test

--- src/Uhuru.CloudFoundry.Test/System/UmbracoTest.cs
+++ src/Uhuru.CloudFoundry.Test/System/UmbracoTest.cs
@@ -239,7 +239,7 @@ namespace Uhuru.CloudFoundry.Test.System
             }
         }
 
-        [TestMethod, Timeout(1000000), TestCategory("System"), Ignore]
+        [TestMethod, Timeout(1000000), TestCategory("System")]
         public void TC005_Create_10Parallel()
         {
             Dictionary<string, string> apps = new Dictionary<string, string>();
