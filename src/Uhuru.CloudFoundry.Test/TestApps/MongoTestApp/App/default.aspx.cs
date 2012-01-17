e18ffbb884a76ceb12a402ad73f2bae9e8206af6
florindragos <florind@uhurusoftware.com>
2012-01-17 16:07:21 +0200

    created test apps for mongodb and redis

---
+++ src/Uhuru.CloudFoundry.Test/TestApps/MongoTestApp/App/default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Data;
+using System.Net;
+
+namespace MongoTestApp
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        string connString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connString = WebConfigurationManager.AppSettings["mongoConnectionString"];
+
+                if (!IsPostBack)
+                {
+                    var db = MongoDatabase.Create(connString);
+                    MongoCollection collection = db.GetCollection("guids");
+
+
+                    var guid = new BsonDocument();
+                    guid["Date"] = DateTime.Now;
+                    guid["Value"] = Guid.NewGuid().ToString();
+                    collection.Insert(guid);
+
+                    MongoCursor<BsonDocument> cursor = collection.FindAllAs<BsonDocument>();
+
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Date", typeof(DateTime));
+                    dt.Columns.Add("Value", typeof(String));
+
+                    foreach (var item in cursor)
+                    {
+                        dt.Rows.Add(item["Date"], item["Value"]);
+                    }
+
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new WebException(ex.ToString(), WebExceptionStatus.ConnectFailure);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connString = WebConfigurationManager.AppSettings["mongoConnectionString"];
+
+            var db = MongoDatabase.Create(connString);
+            MongoCollection collection = db.GetCollection("guids");
+            var guid = new BsonDocument();
+            guid["Date"] = DateTime.Now;
+            guid["Value"] = Guid.NewGuid().ToString();
+            collection.Insert(guid);
+
+            MongoCursor<BsonDocument> cursor = collection.FindAllAs<BsonDocument>();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Date", typeof (DateTime));
+            dt.Columns.Add("Value", typeof(String));
+
+            foreach (var item in cursor)
+            {
+                dt.Rows.Add(item["Date"], item["Value"]);
+            }
+
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string connString = WebConfigurationManager.AppSettings["mongoConnectionString"];
+
+            var db = MongoDatabase.Create(connString);
+            MongoCollection collection = db.GetCollection("guids");
+            collection.RemoveAll();
+
+            MongoCursor<BsonDocument> cursor = collection.FindAllAs<BsonDocument>();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Date", typeof(DateTime));
+            dt.Columns.Add("Value", typeof(String));
+
+            foreach (var item in cursor)
+            {
+                dt.Rows.Add(item["Date"], item["Value"]);
+            }
+
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ src/Uhuru.CloudFoundry.Test/TestApps/RedisTestApp/App/default.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using ServiceStack.Redis;
+using System.Net;
+
+namespace RedisTestApp
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        string redisHost;
+        int redisPort;
+        string redisPassword;
+
+        class Guids
+        {
+            public DateTime Date { get; set; }
+            public Guid Value { get; set; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                redisHost = ConfigurationManager.AppSettings["redisHost"];
+                redisPort = int.Parse(ConfigurationManager.AppSettings["redisPort"]);
+                redisPassword = ConfigurationManager.AppSettings["redisPassword"];
+
+                if (!IsPostBack)
+                {
+                    RedisClient redisClient = new RedisClient(redisHost, redisPort) { Password = redisPassword };
+                    using (var redisGuids = redisClient.GetTypedClient<Guids>())
+                    {
+                        redisGuids.Store(new Guids { Date = DateTime.Now, Value = Guid.NewGuid() });
+                        var allValues = redisGuids.GetAll();
+                        GridView1.DataSource = allValues;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new WebException(ex.ToString(), WebExceptionStatus.ConnectFailure);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string redisHost = ConfigurationManager.AppSettings["redisHost"];
+            int redisPort = int.Parse(ConfigurationManager.AppSettings["redisPort"]);
+            string redisPassword = ConfigurationManager.AppSettings["redisPassword"];
+
+            RedisClient redisClient = new RedisClient(redisHost, redisPort) { Password = redisPassword };
+            using (var redisGuids = redisClient.GetTypedClient<Guids>())
+            {
+                redisGuids.Store(new Guids { Date = DateTime.Now, Value = Guid.NewGuid() } );
+                var allValues = redisGuids.GetAll();
+                GridView1.DataSource = allValues;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string redisHost = ConfigurationManager.AppSettings["redisHost"];
+            int redisPort = int.Parse(ConfigurationManager.AppSettings["redisPort"]);
+            string redisPassword = ConfigurationManager.AppSettings["redisPassword"];
+
+            RedisClient redisClient = new RedisClient(redisHost, redisPort) { Password = redisPassword };
+            using (var redisGuids = redisClient.GetTypedClient<Guids>())
+            {
+                redisGuids.DeleteAll();
+                var allValues = redisGuids.GetAll();
+                GridView1.DataSource = allValues;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
