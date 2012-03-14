05d235e81d2bd9bb6f874edfbcdc172e82b58788
stefans <stefan.schneider@uhurusoftware.com>
2012-03-14 19:13:53 +0200

    commented invalid test

--- src/Uhuru.CloudFoundry.Test/System/Tests.cs
+++ src/Uhuru.CloudFoundry.Test/System/Tests.cs
@@ -6,22 +6,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Uhuru.CloudFoundry.Test.System
 {
-    [TestClass]
-    [DeploymentItem("test.xml")]
-    public class Tests
-    {
-        private TestContext testContextInstance;
+    //[TestClass]
+    //[DeploymentItem("test.xml")]
+    //public class Tests
+    //{
+    //    private TestContext testContextInstance;
 
-        [TestMethod, TestCategory("System")]
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "test.xml", "app", DataAccessMethod.Sequential)]
-        public void SystemTests()
-        {
-            string appName = testContextInstance.DataRow["name"].ToString();
-            string path = testContextInstance.DataRow["path"].ToString();
-            string description = testContextInstance.DataRow["description"].ToString();
-            string services = testContextInstance.DataRow["services"].ToString();
-            string testUrl = testContextInstance.DataRow["testUrl"].ToString();
-            string returnCode = testContextInstance.DataRow["returnCode"].ToString();
-        }
-    }
+    //    [TestMethod, TestCategory("System")]
+    //    [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "test.xml", "app", DataAccessMethod.Sequential)]
+    //    public void SystemTests()
+    //    {
+    //        string appName = testContextInstance.DataRow["name"].ToString();
+    //        string path = testContextInstance.DataRow["path"].ToString();
+    //        string description = testContextInstance.DataRow["description"].ToString();
+    //        string services = testContextInstance.DataRow["services"].ToString();
+    //        string testUrl = testContextInstance.DataRow["testUrl"].ToString();
+    //        string returnCode = testContextInstance.DataRow["returnCode"].ToString();
+    //    }
+    //}
 }
