9640f27ca48995e51c8938749068adf8a85d682e
calinmiclaus <calin.miclaus@uhurusoftware.com>
2012-04-27 12:07:14 +0300

    test for link (commented)

--- src/Uhuru.CloudFoundry.Test/Integration/SambaWindowsClientTest.cs
+++ src/Uhuru.CloudFoundry.Test/Integration/SambaWindowsClientTest.cs
@@ -3,6 +3,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Uhuru.Utilities;
 
 namespace Uhuru.CloudFoundry.Test.Integration
 {
@@ -10,8 +12,56 @@ namespace Uhuru.CloudFoundry.Test.Integration
     public class SambaWindowsClientTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestLink()
         {
+/*
+            //make dir
+            string tempPath1 = Path.GetTempPath();
+            string tempPath2 = Path.GetTempPath();
+            string persistentItemFile=System.IO.Path.GetRandomFileName();
+            string filepath = System.IO.Path.Combine(tempPath1, persistentItemFile);
+            
+            //string persistentItemDir = "temp_folder";
+
+            //create dir
+
+//            if (System.IO.Directory.Exists(tempPath1)) { System.IO.Directory.Delete(tempPath1,true); };
+//            if (System.IO.Directory.Exists(tempPath2)) { System.IO.Directory.Delete(tempPath2,true); };
+
+            if (!System.IO.Directory.Exists(tempPath1)) { System.IO.Directory.CreateDirectory(tempPath1); };
+            if (!System.IO.Directory.Exists(tempPath1)) { System.IO.Directory.CreateDirectory(tempPath1); };
+
+            // create file
+
+            if (System.IO.File.Exists(filepath)) { System.IO.File.Delete(filepath); };
+
+            using (System.IO.FileStream fs = System.IO.File.Create(filepath))
+            {
+                for (byte i = 0; i < 100; i++)
+                {
+                    fs.WriteByte(i);
+                }
+            }
+
+
+            //make link to file
+            SambaWindowsClient.Link(tempPath1, filepath, tempPath2);
+
+            //test if you can access the link to file
+
+
+
+            //make link to dir
+            //test if you can access the link to dir
+
+            //write to file
+            //assert if the content written is the same as the file content
+
+            //test for no overwritten files
+
+            //remove dir
+            //remove file
+ */
         }
     }
 }
