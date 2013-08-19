57a83f0f7368ffd6b29cdf075b7ea27087b5139c
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2013-08-19 15:36:37 +0300

    Add ProcessPrison classes
    Change-Id: I4872c3ab036b79828ca3566a96f8f31552c51bdc

---
+++ src/Uhuru.CloudFoundry.DEA/ProcessPrisonRunInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uhuru.CloudFoundry.DEA
+{
+    public class ProcessPrisonRunInfo
+    {
+
+        public string ExecutablePath
+        {
+            get;
+            set;
+        }
+
+        public Dictionary<string, string> EnvironmentVariables
+        {
+            get;
+            set;
+        }
+
+        public ProcessPrisonRunInfo()
+        {
+        }
+    }
+}
---
+++ src/Uhuru.CloudFoundry.DEA/ProcessPrisonStartInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uhuru.CloudFoundry.DEA
+{
+    public class ProcessPrisonCreateInfo
+    {
+        public bool DiskQuota
+        {
+            get;
+            set;
+        }
+
+        public long MaxMemoryBytes
+        {
+            get;
+            set;
+        }
+
+        public int MaxRunningProcesses
+        {
+            get;
+            set;
+        }
+
+        public ProcessPrisonCreateInfo()
+        {
+            this.DiskQuota = false;
+            this.MaxMemoryBytes = -1;
+            this.MaxRunningProcesses = -1;
+        }
+    }
+}
