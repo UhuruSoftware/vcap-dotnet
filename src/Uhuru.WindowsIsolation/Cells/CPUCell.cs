660f59f293d9e304edc9f30fe397d815fb80e2a8
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2013-09-26 18:30:43 +0300

    Prison refactor by Vlad
    Change-Id: I13f95529fc75f3631488163f60375fbe3e42559e

---
+++ src/Uhuru.WindowsIsolation/Cells/CPUCell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uhuru.WindowsIsolation.Cells
+{
+    class CPUCell : Cell
+    {
+        public override void Lockdown(Prison prison)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CellInstanceInfo[] List()
+        {
+            return new CellInstanceInfo[0];
+        }
+
+        public override void Init()
+        {
+        }
+
+        public override CellType GetFlag()
+        {
+            return CellType.CPU;
+        }
+    }
+}
