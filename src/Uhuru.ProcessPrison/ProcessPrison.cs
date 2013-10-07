902943911e6d5ce507969541bc0afe4994b9bfc8
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2013-10-08 01:51:13 +0300

    CreateProcess delegate progress
    This will allow more processes to be started per prison.
    
    Change-Id: I4f6084f77be8bb5578af26f1ec7b2e43e6f2accb

--- src/Uhuru.ProcessPrison/ProcessPrison.cs
+++ src/Uhuru.ProcessPrison/ProcessPrison.cs
@@ -16,6 +16,7 @@
     using Uhuru.Utilities;
     using Uhuru.Utilities.WindowsJobObjects;
     using System.Runtime.ConstrainedExecution;
+    using System.IO;
 
     public class ProcessPrison
     {
@@ -307,7 +308,6 @@
             private set;
         }
 
-
         public long DiskUsageBytes
         {
             get
@@ -515,7 +515,6 @@
             startupInfo.cb = Marshal.SizeOf(startupInfo.GetType());
             // startupInfo.dwFlags = 0x00000100;            
 
-            string env = BuildEnvironmentVariable(runInfo.EnvironmentVariables);
 
             var creationFlags = ProcessCreationFlags.ZERO_FLAG;
 
@@ -576,23 +575,39 @@
 
                         CreateDesktop(this.WindowsUsername, null, null, 0, ACCESS_MASK.DESKTOP_CREATEWINDOW, null);
 
-                        startupInfo.lpDesktop = string.Format(@"{0}\{0}", this.WindowsUsername);
+                        var envMap = new Dictionary<string, string>();
+                        envMap["Method"] = "CreateProcessWithLogonW";
+                        envMap["Username"] = this.WindowsUsername;
+                        envMap["Domain"] = ".";
+                        envMap["Password"] = this.WindowsPassword;
+                        envMap["LogonFlags"] = ((int)LogonFlags.LOGON_WITH_PROFILE).ToString();
+                        envMap["CommandLine"] = '"' + runInfo.FileName + "\" " + runInfo.Arguments;
+                        envMap["CreationFlags"] = ((int)creationFlags).ToString();
+                        envMap["CurrentDirectory"] = runInfo.WorkingDirectory;
+                        envMap["Desktop"] = string.Format(@"{0}\{0}", this.WindowsUsername);
+
+                        string env = BuildEnvironmentVariable(envMap);
+
+                        // startupInfo.lpDesktop = string.Format(@"{0}\{0}", this.WindowsUsername);
 
                         // Create the process in suspended mode to fence it with a Windows Job Object 
                         // before it executes.
                         // TODO: Use CreateProcessWithToken to prevent Windows for creating an unamed job object for the
                         // second created process.
                         // http://stackoverflow.com/questions/1287620/createprocesswithlogonw-and-assignprocesstojobobject
-                        bool ret = CreateProcessWithLogon(
-                            this.WindowsUsername,
-                            this.WindowsDomain,
-                            this.WindowsPassword,
-                            LogonFlags.LOGON_WITH_PROFILE,
-                            runInfo.FileName,
-                            runInfo.Arguments,
+
+                        string processPath = Process.GetCurrentProcess().MainModule.FileName;
+                        string processDirPath = Directory.GetParent(processPath).FullName;
+
+                        bool ret = CreateProcess(
+                            null,
+                            processDirPath + @"\Uhuru.WindowsIsolation.CreateProcessDelegate.exe",
+                            IntPtr.Zero,
+                            IntPtr.Zero,
+                            true,
                             creationFlags,
                             env,
-                            runInfo.WorkingDirectory,
+                            null,
                             ref startupInfo,
                             out processInfo
                             );
@@ -760,12 +775,15 @@
             {
                 foreach (var EnvironmentVariable in EvnironmantVariables)
                 {
-                    if (EnvironmentVariable.Key.Contains('=') || EnvironmentVariable.Key.Contains('\0') || EnvironmentVariable.Value.Contains('\0'))
+                    var value = EnvironmentVariable.Value;
+                    if (value == null) value = "";
+
+                    if (EnvironmentVariable.Key.Contains('=') || EnvironmentVariable.Key.Contains('\0') || value.Contains('\0'))
                     {
                         throw new ArgumentException("Invalid or restricted charachter", "EvnironmantVariables");
                     }
 
-                    ret += EnvironmentVariable.Key + "=" + EnvironmentVariable.Value + '\0';
+                    ret += EnvironmentVariable.Key + "=" + value + '\0';
                 }
 
 
